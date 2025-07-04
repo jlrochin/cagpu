42c190801813699bc954d5e54e951e4824ca5ca0
jlrupton <jlrupton@icloud.com>
2025-07-04 17:37:11 -0600

    Eliminación del archivo .gitignore.save y actualización de las dependencias en package.json y pnpm-lock.yaml. Se eliminaron dependencias no utilizadas y se optimizó la estructura del archivo de bloqueo, mejorando la gestión de paquetes del proyecto. Además, se modificó el hook useMediaQuery para utilizar el evento 'change' en lugar de 'resize', mejorando la eficiencia en la detección de cambios de media.

--- hooks/use-media-query.ts
+++ hooks/use-media-query.ts
@@ -11,8 +11,8 @@ export function useMediaQuery(query: string): boolean {
       setMatches(media.matches)
     }
     const listener = () => setMatches(media.matches)
-    window.addEventListener("resize", listener)
-    return () => window.removeEventListener("resize", listener)
+    media.addEventListener("change", listener)
+    return () => media.removeEventListener("change", listener)
   }, [matches, query])
 
   return matches
